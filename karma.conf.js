05717371f032bc592032ebbda37babfc0e003132
trapple <trapplejp@gmail.com>
2014-04-22 16:40:46 +0900

    change karma reporter to spec

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
